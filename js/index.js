80944dcaea8ddb7d9e18e847a7acfcc6a666ba02
MarcosBordeu <mbordeu@udesa.edu.ar>
2021-06-08 11:28:59 -0300

    simplificamos el html

--- js/index.js
+++ js/index.js
@@ -10,13 +10,19 @@ fetch("")
     let album = document.querySelector(".album")
     let artista = document.querySelector(".artista")
 
-    tracks.forEach(function(canciones) {
-        console.log(canciones.title);
-        cancion.innerHTML += '<nav class="carta"><img class="imgl" src="'+ lacancion.imagen +'" alt="'+ lacancion.nombre +'">  </nav> <h3><a href="'+ +'"> ' + lacancion.nombre +'</a></h3>     <p>Cancion que pertenece al album <a href="'+ lacancion.album +'"> <b>'+ lacancion.album.nombre +'</b></a> de <a href="'+lacancion.artista.nombre+'.html"> <b>Mac Miller</b></a></p>'              
-    });
-    for (let i = 0; i < topalbums.length; i++) {
+    for (let i = 0; i < topcanciones.length; i++) {
+        cancion.innerHTML += '<nav class="carta"><img class="imgl" src="'+ lacancion.imagen +'" alt="'+ lacancion.nombre +'">  </nav> <h3><a href="'+ +'"> ' + lacancion.nombre +'</a></h3>     <p>Cancion que pertenece al album <a href="'+ lacancion.album +'"> <b>'+ lacancion.album.nombre +'</b></a> de <a href="'+lacancion.artista.nombre+'.html"> <b>Mac Miller</b></a></p>'
+    }
+                      
+    for (let i = 0; i < topalbumes.length; i++) {
         album.innerHTML += '<nav class="carta"><a href="'+ album.nombre +'.html"><img class="imgl" src="'+ album.imagen +'" alt="'+ album.nombre+' by '+ album.artista+'"></a></nav><h3><a href="'+ album.nombre+'.html">'+album.nombre+'</a></h3><p> Album lanzado en el año '+album.anio+'por <a href="'+album.artista+'.html"><b>'+album.artista+'</b></a></p>'
         
     }
     
+    for (let i = 0; i < topartistas.length; i++) {
+        artista.innerHTML += '<nav class="carta"><a href="'+ artista.nombre +'.html"><img class="imgl" src="'+ artista.imagen +'" alt="'+ artista.nombre +'"> </a> </nav><h3><a href="'+ artista.nombre +'".html">'+ artista.nombre +'</a></h3><p>'+ artista.oyentes +'oyentes mensuales</p></nav>'
+        
+    }
 })
+
+
